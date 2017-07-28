8e2f1215af5953a335b0b29fcfbb68ba45249657
mac <mac@macs-iMac-2.local>
2017-07-28 18:47:48 +0530

    QRCodeGenerator demo

--- QRcodeGeneratorDemo/ViewController.swift
+++ QRcodeGeneratorDemo/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txt_enterbyuser: UITextField!
+    
+    @IBOutlet weak var img_myimage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btn_convert(_ sender: Any) {
+        if let mystring = txt_enterbyuser.text{
+            let data = mystring.data(using: .ascii, allowLossyConversion: true)
+            let filter = CIFilter(name: "CIQRCodeGenerator")
+            filter?.setValue(data, forKey: "inputMessage")
+            
+            let image = UIImage(ciImage: (filter?.outputImage)!)
+            img_myimage.image = image
+        }
+    }
 
 }
 
